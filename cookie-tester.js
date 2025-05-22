1284f339291f8b814dc37ca522a1418b72dc1a8f
mr-stamp <nathanael.stamp@youngsvilleacademy.org>
2025-05-22 10:25:40 -0400

    Update cookie-tester.js

--- cookie-tester.js
+++ cookie-tester.js
@@ -1,3 +1,13 @@
-document.cookie = "test=1; max-age=31536000";
+
+//parse out cookie to see if it has initial access
+let cookieArr = document.cookie.split(";");
+let hasInitAccess = false;
+
+for (const el of cookieArr) {
+  if (el.split("=")[0] == "init-access") hasInitAccess = true;
+}
+
+if (!hasInitAccess) document.cookie = "init-access=" + new Date() + "; max-age=31536000";
+else document.cookie = "new-access=" + new Date() + "; max-age=31536000";
 
 console.log(document.cookie);
